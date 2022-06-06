19d875fe19a54ca78736b0b2ebb06993e37c0d7b
adinrichter <adinrichter@gmail.com>
2022-06-06 16:13:06 -0700

    Added challenge

---
+++ Basic JS Projects/Expressions Alert/JS/main.js
@@ -0,0 +1,2 @@
+window.alert("Welcome to my webiste!") // welcomes the user to the website
+document.write("There's not much here yet, but come back in ", 3+3, " days!") // tells the user to check back in soon
