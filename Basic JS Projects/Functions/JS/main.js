9157159868051589ba9022695f2753703daeb054
adinrichter <adinrichter@gmail.com>
2022-06-09 14:17:33 -0700

    Modified main.js to include a +=

--- Basic JS Projects/Functions/JS/main.js
+++ Basic JS Projects/Functions/JS/main.js
@@ -3,9 +3,10 @@ var num; // creates a number variable to be used later
 function pressed() { 
     // counts how many times the user has pressed the button, and sets that to the text of the button
     if (num >= 1) { 
-        num = num + 1;
-    } else {
-        num = 1;
+        num += 1
+    }
+    else {
+        num = 1
     }
     str = "Clicks: " + num;
     document.getElementById("button").innerHTML = str;
