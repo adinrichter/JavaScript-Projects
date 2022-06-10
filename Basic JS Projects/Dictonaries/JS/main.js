6fba6cd0f50025b7c8202d4d3f6e40f22c781be6
adinrichter <adinrichter@gmail.com>
2022-06-09 18:41:34 -0700

    Created Dictonaries folder and cleaned up Math Operators main.js code (added semicolons)

---
+++ Basic JS Projects/Dictonaries/JS/main.js
@@ -0,0 +1,70 @@
+// initializes a simple dictionary
+var dict = {
+    Name : "Adin R",
+    Age : 18,
+    Job : "Student",
+    Skills : "Programming"
+}
+
+// displays the current dictionary when called
+function displayDict() {
+    // initalizes variables to be used
+    const keys = Object.keys(dict);
+    const values = Object.values(dict);
+    let out = "";
+    // creates a list of all items in the dictionary and separates them with a line break
+    for (let i = 0; i < keys.length; i++) {
+        out += `${keys[i]}: ${values[i]}<br>`;
+    }    
+    // displays the dictionary at the given location
+    document.getElementById("dictionary").innerHTML = out;
+}
+
+// displays inital dictionary on load
+displayDict()
+
+// removes user given entry if it exists
+function removeEntry() {
+    // initalizes constants to be used
+    const key = document.getElementById("removeEntry");
+    const dic = dict;
+    // checks the user's value against the current dictionary and deletes it if it was found
+    if (key.value in dic) {
+        delete dic[key.value];
+    // clears current input field on success
+        key.value = "";
+    }
+    // displays an error if the entry wasn't found
+    else {
+        alert("Entry not found (note: case sensitive)");
+    }
+    // updates the currently displayed dictionary
+    displayDict();
+}
+
+// adds user given entry if the key does not yet exist
+function addEntry() {
+    // initializes constants to be used
+    const key = document.getElementById("addKey");
+    const value = document.getElementById("addValue");
+    // checks if key already exists and shows an error if it does
+    if (key.value in dict) {
+        alert("The entry provided already has an existing occurance");
+    }
+    // adds user given KVP if it is complete
+    else {
+        if (key.value != "" && value.value != "") {
+            const dic = dict;
+            dic[key.value] = value.value;
+    // clears current input field on success
+            key.value = " ";
+            value.value = " ";
+        }
+        else {
+    // displays an error if there are empty key or value entries
+            alert("You must provide a key and value for your entry");
+        }
+    }
+    // updates the currently displayed dictionary
+    displayDict();
+}
--- Basic JS Projects/Math Operators/JS/main.js
+++ Basic JS Projects/Math Operators/JS/main.js
@@ -4,13 +4,13 @@ function addForm() {
     var2 = parseInt(document.getElementById("var2").value);
     // checks if inputs are numbers, and adds them if they are
     if (parseInt(var1+var2)) {
-        result = var1+var2
+        result = var1+var2;
     // displays the result on the main page
         document.getElementById("result").innerHTML = var1 + " + " + var2 + " is " + result;
     }
     // returns an error message if the inputs are not both numbers
     else {
-        document.getElementById("result").innerHTML = "Your result could not be displayed, you may have 1 or more invalid inputs."
+        document.getElementById("result").innerHTML = "Your result could not be displayed, you may have 1 or more invalid inputs.";
     } 
 }
 
@@ -22,17 +22,17 @@ function subtractForm() {
     if (parseInt(var1+var2)) {
     //checks if the result is positive or negative, and returns relevant message
         if ((var1-var2) < 0) {
-            result = "negative " + -(var1-var2)
+            result = "negative " + -(var1-var2);
         }
         else {
-            result = var1-var2
+            result = var1-var2;
         }
     // displays the result on the main page
         document.getElementById("result").innerHTML = var1 + " - " + var2 + " is " + result;
     }
     // returns an error message if the inputs are not both numbers
     else {
-        document.getElementById("result").innerHTML = "Your result could not be displayed, you may have 1 or more invalid inputs."
+        document.getElementById("result").innerHTML = "Your result could not be displayed, you may have 1 or more invalid inputs.";
     }
 }
 
@@ -43,21 +43,21 @@ function divideForm() {
     // checks if inputs are numbers, and divides the first by the second if they are
     if (parseInt(var1+var2)) {
     // determines the remainder and quotient
-        result = Math.floor(var1/var2)
-        remainder = var1 % var2
+        result = Math.floor(var1/var2);
+        remainder = var1 % var2;
     // determines how to display result
     if (result >= -1 && result <= 1 && result != 0) {
-        result = result + " time,"
+        result = result + " time,";
     }
     else {
-        result = result + " times,"
+        result = result + " times,";
     }
     // displays the result on the main page
         document.getElementById("result").innerHTML = var2 + " goes into " + var1 + " " + result + " with a remainder of " + remainder;
     }
     // returns an error message if the inputs are not both numbers
     else {
-        document.getElementById("result").innerHTML = "Your result could not be displayed, you may have 1 or more invalid inputs."
+        document.getElementById("result").innerHTML = "Your result could not be displayed, you may have 1 or more invalid inputs.";
     }
 }
 
@@ -67,13 +67,13 @@ function multiplyForm() {
     var2 = parseInt(document.getElementById("var2").value);
     // checks if inputs are numbers, and multiplies them if they are
     if (parseInt(var1+var2)) {
-        result = var1*var2
+        result = var1*var2;
     // displays the result on the main page
         document.getElementById("result").innerHTML = var1 + " * " + var2 + " is " + result;
     }
     // returns an error message if the inputs are not both numbers
     else {
-        document.getElementById("result").innerHTML = "Your result could not be displayed, you may have 1 or more invalid inputs."
+        document.getElementById("result").innerHTML = "Your result could not be displayed, you may have 1 or more invalid inputs.";
     }
 }
 
@@ -83,20 +83,20 @@ function pythagoreanForm() {
     var2 = parseInt(document.getElementById("var2").value);
     // checks if inputs are numbers, and multiplies them if they are
     if (parseInt(var1+var2)) {
-        result = Math.sqrt(var1**2+var2**2)
+        result = Math.sqrt(var1**2+var2**2);
     // displays the result on the main page
         document.getElementById("result").innerHTML = "The hypotenuse of a right triangle with side lengths " + var1 + " and " + var2 + " is " + result;
     }
     // returns an error message if the inputs are not both numbers
     else {
-        document.getElementById("result").innerHTML = "Your result could not be displayed, you may have 1 or more invalid inputs."
+        document.getElementById("result").innerHTML = "Your result could not be displayed, you may have 1 or more invalid inputs.";
     }
 }
 
 function incrementVar1() {
     // creates a constant to modify operator 1
     const num = document.getElementById("var1");
-    out = num.value
+    out = num.value;
     // increases value by 1 if there's currently a number, sets it to 1 if there isn't
     if (parseInt(out)) {
         num.value = ++out;
@@ -109,7 +109,7 @@ function incrementVar1() {
 function decrementVar1() {
     // creates a constant to modify operator 1
     const num = document.getElementById("var1");
-    out = num.value
+    out = num.value;
     // decreases value by 1 if there's currently a number, sets it to -1 if there isn't
     if (parseInt(out)) {
         num.value = --out;
@@ -122,7 +122,7 @@ function decrementVar1() {
 function incrementVar2() {
     // creates a constant to modify operator 2
     const num = document.getElementById("var2");
-    out = num.value
+    out = num.value;
     // increases value by 1 if there's currently a number, sets it to 1 if there isn't
     if (parseInt(out)) {
         num.value = ++out;
@@ -135,7 +135,7 @@ function incrementVar2() {
 function decrementVar2() {
     // creates a constant to modify operator 2
     const num = document.getElementById("var2");
-    out = num.value
+    out = num.value;
     // decreases value by 1 if there's currently a number, sets it to -1 if there isn't
     if (parseInt(out)) {
         num.value = --out;
@@ -147,12 +147,12 @@ function decrementVar2() {
 
 function randomVar1() {
     // sets the value of operator 1 to a random number between 0 and 999
-    const num = document.getElementById("var1")
-    num.value = Math.floor(Math.random() * 1000)
+    const num = document.getElementById("var1");
+    num.value = Math.floor(Math.random() * 1000);
 }
 
 function randomVar2() {
     // sets the value of operator 2 to a random number between 0 and 999
-    const num = document.getElementById("var2")
-    num.value = Math.floor(Math.random() * 1000)
+    const num = document.getElementById("var2");
+    num.value = Math.floor(Math.random() * 1000);
 }
